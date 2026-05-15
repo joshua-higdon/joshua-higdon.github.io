a83ca2d45364149b29c9853b6c695fa4e38c002c
joshua-higdon <134975642+joshua-higdon@users.noreply.github.com>
2026-05-15 16:14:06 +0000

    Deploying to gh-pages from @ joshua-higdon/joshua-higdon.github.io@25f83489c481aa008986b81a63e0f718918d8047 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+          description: "Courses taught at the University of Florida organized by course level.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
