83ae6db46f0b9a6f5de6ac1a9a576d6017851d65
joshua-higdon <134975642+joshua-higdon@users.noreply.github.com>
2026-05-15 19:28:02 +0000

    Deploying to gh-pages from @ joshua-higdon/joshua-higdon.github.io@cc200a183fd949c0e1bd19821384cddc6192f406 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,9 +422,9 @@ ninja.data = [{
           description: "Image credit-Spanish Grammar in Context",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-person-agreement-across-the-preterite-and-present-perfect-tenses-in-spanish-heritage-speakers",
+          title: 'Person agreement across the preterite and present perfect tenses in Spanish heritage speakers...',
+          description: "Image credit-Baselang",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
